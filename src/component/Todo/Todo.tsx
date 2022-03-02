a01732e935c2cbe161c17951cb01eedb1b65cb23
望月祐汰 <mczk9402@gmail.com>
2022-03-03 00:28:30 +0900

    スワイプで削除ボタン出現、 butonタップ時のフォーカス機能を追加、 スワイプで削除ボタン出現 #12

--- src/component/Todo/Todo.tsx
+++ src/component/Todo/Todo.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 interface Props {
   todo: string;
@@ -6,11 +7,44 @@ interface Props {
 
 export const Todo: React.VFC<Props> = ({ todo }) => {
   const [active, setActive] = useState(false);
+  const [focus, setFocus] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => {
+      console.log('User Swiped!', eventData);
+      if (eventData.dir === 'Left') {
+        setOpen(true);
+      } else if (eventData.dir === 'Right') {
+        setOpen(false);
+      }
+
+      console.log(open);
+      return false;
+    },
+  });
 
   return (
-    <div>
-      <input type='checkbox' onChange={() => setActive((prev) => !prev)} />
-      <button className={`${active ? 'line-through' : undefined}`}>{todo}</button>
-    </div>
+    <button
+      {...handlers}
+      className={`${focus ? 'bg-yellow-50' : ''} grid grid-flow-col justify-between  w-full `}
+      onClick={() => {
+        setFocus((prev) => !prev);
+        setOpen(false);
+      }}
+    >
+      <div>
+        <input type='checkbox' onChange={() => setActive((prev) => !prev)} />
+        <span className={active ? 'line-through' : ''}>{todo}</span>
+      </div>
+      <div className='relative overflow-hidden'>
+        <button className='bg-red-500 text-white px-2'>削除</button>
+        <div
+          className={`${
+            open && !focus ? '-translate-x-full' : ''
+          } bg-white absolute top-0 left-0 w-full h-full transition-transform duration-200`}
+        ></div>
+      </div>
+    </button>
   );
 };
