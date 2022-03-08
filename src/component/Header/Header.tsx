401e6b04dee7f709967809f2e8b9f2a9bb70a897
mittiy <rouezura@yahoo.co.jp>
2022-03-08 21:49:55 +0900

    Header作成しました https://github.com/qin-todo-team-l/qin-todo-frontend/issues/1

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='max-w-5xl mx-auto flex justify-between items-center h-22'>
+      <div>
+        {/* <img src='/logo.svg' alt='Qin Todo' /> */}
+        <Image src='/logo.svg' alt='Qin Todo' width={100} height={100} />
+        {/* https://nextjs.org/docs/api-reference/next/image */}
+      </div>
+      <div>
+        <Image src='/person.png' alt='personLogo' width={36} height={36} />
+      </div>
+    </header>
+  );
 };
